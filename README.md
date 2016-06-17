f9f542ddc10c770e5c24b0816ffee179094f06e8
Vitaly R. Samigullin <vrs@pilosus.org>
2016-06-17 11:25:30 +0200

    More efficient implementation of the function; typos fixed

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# interview-puzzles
+# Puzzles
+
+## Ya
+
+Checking execution time with `timeit`
+
+$ python3 -m timeit -n 100 "import menu; menu.generate_menu(365)"
+100 loops, best of 3: 25.8 msec per loop
+
+$ python3 -m timeit -n 100 "import menu; menu.generate_menu2(365)"
+100 loops, best of 3: 12.3 msec per loop
--- ya/menu.py
+++ ya/menu.py
@@ -5,10 +5,10 @@ import bisect
 import itertools
 
 ### Constants
-MAINS = {'chiken': 0.3, 'beef': 0.3, 'fish': 0.25, 'pork': 0.15}
-STAPLES = {'rice': 0.2, 'buckwheat': 0.15, 'past': 0.04,
+MAINS = {'chicken': 0.3, 'beef': 0.3, 'fish': 0.25, 'pork': 0.15}
+STAPLES = {'rice': 0.2, 'buckwheat': 0.15, 'pasta': 0.04,
            'potato': 0.01, 'vegs': 0.6}
-SNACKS = {'bread': 0.1, 'chese': 0.15, 'mayonnese': 0.03, 'none': 0.72}
+SNACKS = {'bread': 0.1, 'cheese': 0.15, 'mayonnese': 0.03, 'none': 0.72}
 
 PLATE =  """
                       ___         
@@ -65,6 +65,8 @@ def random_pick(weighted_choices: (str, int)) -> str:
     
 def generate_menu(days: int) -> [Menu]:
     """Return list of Menu for number of days specified.
+
+    days >= 2
     """
     mains = list(map(lambda n: (n, round(MAINS[n] * days)), MAINS))
     staples = list(map(lambda n: (n, round(STAPLES[n] * days)), STAPLES))
@@ -81,7 +83,32 @@ def generate_menu(days: int) -> [Menu]:
 
     return menu_list
 
+def distribution(weighted_choices: (str, int)) -> [str]:
+    return [val for val, cnt in weighted_choices for i in range(cnt)]
+    
+def generate_menu2(days: int) -> [Menu]:
+    """Return list of Menu for number of days specified.
+
+    Faster implementation, as distribution lists get generated once.
+
+    days >= 2
+    """
+    menu_list = []
+
+    mains = distribution(map(lambda n: (n, round(MAINS[n] * days)), MAINS))
+    staples = distribution(map(lambda n: (n, round(STAPLES[n] * days)), STAPLES))
+    snacks = distribution(map(lambda n: (n, round(SNACKS[n] * days)), SNACKS))
+
+    while(days):
+        menu = Menu(random.choice(mains), random.choice(staples),
+                    random.choice(snacks))
+        if menu.is_digestable():
+            menu_list.append(menu)
+            days -=1
+
+    return menu_list
+
 if __name__ == '__main__':
-    list = generate_menu(365)
-    for i in list:
+    menu = generate_menu2(365)
+    for i in menu:
         print(i)
