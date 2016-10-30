01e6079628fe79d6d2adbfc41e8b467bad2d091f
Vitaly R. Samigullin <vrs@pilosus.org>
2016-10-30 12:53:25 +0300

    Comments and style fixed

--- ra/ma.py
+++ ra/ma.py
@@ -1,27 +1,59 @@
 # -*- coding: utf-8 -*-
 
 """
-Coding time: 59 min
+ma is a module for Ramax International's interview test problem.
 
+This module shows the use of class inheritance in Python, setting and
+getting object's attributes, as well as throwing exceptions.
 
-instanceof 
+The code below follows PEP8 style guide for Python code and
+checked against pep8 tool.
 
-see: 
-
-https://stackoverflow.com/questions/1549801/differences-between-isinstance-and-type-in-python
+Total coding time for ma and mb modules: 59 minutes.
+Text editor: GNU/Emacs
 """
 
+__author__ = 'Vitaly R. Samigullin'
+__copyright__ = "Copyright 2016, Vitaly R. Samigullin"
+__status__ = 'Production'
+
+
+class MyError(Exception):
+    """
+    A custom error class derived from the Exception class.
+
+    It used to throw a custom exception in the First class fnc method.
+    """
+    pass
+
+
 class Parent(object):
+    """
+    A parent class the following classes derived from.
+
+    There's nothing to do in this class. Let's just pass it.
+    """
     pass
-        
+
+
 class First(Parent):
+    """
+    A subclass of a Parent class
+
+    We could redefine constructor as follows:
+
+    def __init__(self):
+        super(Parent, self).__init__()
+
+    But let's stick to a Python's concise and beautiful style and
+    just pass the body of the derived class
+    """
     pass
+
 
 class Second(Parent):
     pass
 
-class MyError(Exception):
-    pass
 
 class A(First):
     def __init__(self):
@@ -29,18 +61,29 @@ class A(First):
         self.isSecond = 0
 
     def __setattr__(self, name, value):
+        # throw an exception if and only if
+        # we try to assign 2 to isSecond attribute
         if name == 'isSecond' and value == 2:
             raise AttributeError()
         else:
             object.__setattr__(self, name, value)
-        
+
     def fnc(self, val):
         if val == 7:
             raise MyError('Error text')
         else:
+            # we could easily use 3 instead of self.i
+            # because setting object's attribute i to another value
+            # would break the test, but we don't since using `magic` numbers
+            # considered a bad style of programming.
             return val * val * self.i
 
     def isFirst(self):
-        return 1
+        """
+        isFirst breaks PEP8 naming conventions (this is not Java!),
+        but probably makes a good job tricking down an interviewee.
 
-    
+        The good name for this function could be isfirst (like isinstance),
+        or is_first.
+        """
+        return 1
--- ra/mb.py
+++ ra/mb.py
@@ -2,14 +2,40 @@
 
 from ma import Second
 
+"""
+mb is a module for Ramax International's interview test problem.
+
+This module shows the use of class inheritance in Python, defining
+class methods, and commenting.
+
+The code below follows PEP8 style guide for Python code and
+checked against pep8 tool.
+
+Total coding time for ma and mb modules: 59 minutes.
+Text editor: GNU/Emacs
+"""
+
+__author__ = 'Vitaly R. Samigullin'
+__copyright__ = "Copyright 2016, Vitaly R. Samigullin"
+__status__ = 'Production'
+
+# Common sense and PEP8 require to use descriptive names for constants,
+# variables, functions, etc. We do not want to have some magic coefficients
+# in the fnc method. We don't now either what fnc exactly doing or should it
+# rely on a object's attribute of some kind (as we supposed in ma module).
+# So for the diversity sake let's assume this time, that fnc uses a constant.
+# We still have no idea what it's for, but bear in mind it should be named
+# properly!
+NOT_A_MAGIC_CONSTANT = 5
+
+
 class B(Second):
     def __init__(self, i):
         self.i = i
-        self.magic = 5
         self.isSecond = 1
 
     def fnc(self, val1, val2):
-        return val1 * val2 * self.magic
+        return val1 * val2 * NOT_A_MAGIC_CONSTANT
 
     def isFirst(self):
         return 0
